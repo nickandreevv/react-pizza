932bdea0b7adf9eafaa4d39ec9e81881981b8c34
nickandreev <nandreev294@gmail.com>
2023-04-18 15:45:29 +0300

    added pagination using npm paginate

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -3,11 +3,13 @@ import Categories from '../Categories'
 import Sort from '../Sort'
 import Skeleton from '../PizzaBlock/Skeleton'
 import PizzaBlock from '../PizzaBlock/PizzaBlock'
+import Pagination from '../Pagination/Pagination'
 
 const Home = ({ value }) => {
   const [items, setItems] = useState([]) // получаем пустой массив, в который будем добавлять данные с бэка
   const [categoryId, setCategoryId] = useState(0) // начальный индекс списка
   const [isLoading, setIsLoading] = useState(true) // загрузочное окно
+  const [currentPage, setCurrentPage] = useState(1) // пагинация страниц. Первое значение стейта - первая страница в списке
   const [sort, setSort] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -27,15 +29,15 @@ const Home = ({ value }) => {
     const category = categoryId > 0 ? `category=${categoryId}` : '' // фильтр по категориям
     const search = value ? `&search=${value}` : '' // при изменении инпута оставляем те пиццы, которое подходят под тебования
     fetch(
-      `https://63f4babd3f99f5855db60353.mockapi.io/pizzas?${category}&sortBy=${sort.sortProperty}&order=desc${search}`
+      `https://63f4babd3f99f5855db60353.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sort.sortProperty}&order=desc${search}`
     ) // делаем запрос. При измененни переменных - делаем запрос с нужными данными. По сортировке
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr) // добавляем в массив данные с бэка
         setIsLoading(false) // меняем загрузочное окно на загрузившийся массив
       })
-    window.scrollTo(0, 0) // при загрузке страницы скролл будет идти с самого верха
-  }, [categoryId, sort, value]) // если какая-то переменная меняется - делает запрос
+    // window.scrollTo(0, 0) // при загрузке страницы скролл будет идти с самого верха. UPD: скролл убрал, т.к на странице оставляю по 4 товара. Смысла в нем нет
+  }, [categoryId, sort, value, currentPage]) // если какая-то переменная меняется - делает запрос
 
   const skeletons = [...new Array(9)].map((_, index) => (
     <Skeleton key={index} />
@@ -57,6 +59,7 @@ const Home = ({ value }) => {
           ? skeletons //  при загрузке создаем массив undef , и перебираем его
           : pizzas}
       </div>
+      <Pagination onChangePage={(num) => setCurrentPage(num)} />
     </div>
   )
 }
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,19 @@
-const Pagination = () => {
-  return <div>Pagination</div>
+import ReactPaginate from 'react-paginate'
+import s from './Pagination.module.scss'
+
+const Pagination = ({ onChangePage }) => {
+  return (
+    <ReactPaginate
+      className={s.root}
+      breakLabel="..."
+      nextLabel=" >"
+      previousLabel="< "
+      onPageChange={(event) => onChangePage(event.selected + 1)}
+      pageRangeDisplayed={4}
+      pageCount={3}
+      renderOnZeroPageCount={null}
+    />
+  )
 }
 
 export default Pagination
