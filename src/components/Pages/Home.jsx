7f08a73c296d0504065b7867767ac03294c640f0
nickandreev <nandreev294@gmail.com>
2023-06-07 22:11:44 +0300

    continue making project

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -10,35 +10,29 @@ import Pagination from '../Pagination/Pagination'
 import { useContext } from 'react'
 import { MainContext } from '../../App'
 import { useDispatch, useSelector } from 'react-redux'
-import { setItems } from '../../redux/slices/pizzasSlice'
+import { setItems, fetchPizzas } from '../../redux/slices/pizzasSlice'
 
 const Home = () => {
   const categoryId = useSelector((state) => state.filter.categoryId) // получение начальной категории при загрузке страницы. Изменение прописано в Categories
   const sort = useSelector((state) => state.filter.sort.sortProperty) // получение начального значения сортировки при загрузке страницы. Изменение прописано в Sort
   const currentPage = useSelector((state) => state.filter.currentPage)
   const { value } = useContext(MainContext) // значение инпута из компонента Search
-  const [isLoading, setIsLoading] = useState(true) // загрузочное окно
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const items = useSelector((state) => state.pizzas.items) // начальный стейт с редакса. Пустой массив
+  const { items, status } = useSelector((state) => state.pizzas) // начальный стейт с редакса. Пустой массив и начальное значение для загрузки
 
-  const fetchPizzas = async () => {
-    setIsLoading(true)
-
+  const getPizzas = async () => {
     const category = categoryId > 0 ? `category=${categoryId}` : '' // фильтр по категориям
     const search = value ? `&search=${value}` : '' // при изменении инпута оставляем те пиццы, которое подходят под требования
 
-    try {
-      const mockapiResponce = await axios.get(
-        `https://63f4babd3f99f5855db60353.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}${search}&sortBy=${sort}&order=desc`
-      ) // избавляемся от then с помощью await. Данные сразу записываются в переменную
-      dispatch(setItems(mockapiResponce.data)) // при рендере в функцию передаем ответ с бэка. И массив заполняется данными
-    } catch (error) {
-      console.log(error, 'ERROR')
-    } finally {
-      setIsLoading(false) // выполнится в любом случае. Т.к setIsLoading в любом случае
-      // делаем false - то можем добавить его сюда. И он будет false в любом из 2 вариантов
-    }
+    dispatch(
+      fetchPizzas({
+        category,
+        search,
+        currentPage,
+        sort,
+      })
+    ) // UPD: убрал try/catch т.к все запросы идут через редакс и обработка будет в extraReducers
     window.scroll(0, 0) // при загрузке страницы скролл будет идти с самого верха.
   }
 
@@ -51,7 +45,7 @@ const Home = () => {
   }, [categoryId, sort, value, currentPage]) // когда делаем qs - всегда пихаем его в useEffect и следим за переменными
 
   useEffect(() => {
-    fetchPizzas()
+    getPizzas()
   }, [categoryId, sort, value, currentPage]) // если какая-то переменная меняется - useEffect ререндерит
 
   const skeletons = [...new Array(9)].map((_, index) => (
@@ -69,11 +63,19 @@ const Home = () => {
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? skeletons //  при загрузке создаем массив undef , и перебираем его
-          : pizzas}
-      </div>
+      {status === 'error' ? (
+        <>
+          <h2>Произошла ошибка 😕</h2>
+          <p>Не удалось получить пиццы</p>
+        </>
+      ) : (
+        <div className="content__items">
+          {status === 'loading'
+            ? skeletons //  при загрузке создаем массив undef , и перебираем его
+            : pizzas}
+        </div>
+      )}
+
       <Pagination />
     </div>
   )
--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -1,7 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+export const fetchPizzas = createAsyncThunk(
+  'pizza/fetchPizzasStatus',
+  async (params) => {
+    const { category, search, currentPage, sort } = params
+    const mockapiResponce = await axios.get(
+      `https://63f4babd3f99f5855db60353.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}${search}&sortBy=${sort}&order=desc`
+    )
+    return mockapiResponce.data // перенесли логику отправки запроса в редакс для того, чтобы переиспользовать функцию
+    // и в комп. home мы просто ТУПО будем вызывать функ. fecthPizzas в dispatch
+  }
+) // pizza/fetchPizzasStatus - префикс создается для того, чтобы редакс корректно определял какое действие нужно сделать
 
 const initialState = {
   items: [],
+  status: 'loading', // загрузка | выполнено | отклонено
 }
 
 const pizzasSlice = createSlice({
@@ -13,6 +26,20 @@ const pizzasSlice = createSlice({
     },
   }, // выносим логику локального state из Home сюда.
   // тут и будем добавлять в пустой массив пиццы из бэка
+  extraReducers: {
+    [fetchPizzas.fulfilled]: (state, action) => {
+      state.items = action.payload
+      state.status = 'success'
+    },
+    [fetchPizzas.pending]: (state, action) => {
+      state.status = 'loading'
+      state.items = []
+    },
+    [fetchPizzas.rejected]: (state, action) => {
+      state.status = 'error'
+      state.items = []
+    },
+  }, // после того, как мы сделали запрос и чтобы данные появлялись, нужно писать extraReducers
 })
 
 export const { setItems } = pizzasSlice.actions
